2a0bb94ef92f3d9364e434f8e1d7752fc3cc9665
Matt Moore <mattmoor@chainguard.dev>
2022-07-14 19:23:28 -0700

    Fix the output text for Slack URLs (#2145)

--- slack-webhook/iac/outputs.tf
+++ slack-webhook/iac/outputs.tf
@@ -1,4 +1,4 @@
 output "secret-command" {
-  description = "gcloud secrets command to upload the github personal access token."
-  value       = format("echo -n YOUR GITHUB PAT | gcloud --project %s secrets versions add %s --data-file=-", var.project_id, google_secret_manager_secret.slack-url.secret_id)
+  description = "gcloud secrets command to upload the Slack webhook URL."
+  value       = format("echo -n YOUR SLACK WEBHOOK | gcloud --project %s secrets versions add %s --data-file=-", var.project_id, google_secret_manager_secret.slack-url.secret_id)
 }
