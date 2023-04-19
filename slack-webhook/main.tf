990d511febcba13d55a87a26ac505751103bcd97
Jamon Camisso <jamonation@users.noreply.github.com>
2023-04-19 18:18:14 -0400

    Delete main.tf

--- slack-webhook/main.tf
+++
@@ -1,6 +0,0 @@
-module "slack-webhook" {
-  source     = "github.com/chainguard-dev/enforce-events/slack-webhook/iac"
-  name       = "enforce-events"
-  project_id = "jamon-chainguard"
-  group      = "jamon-test"
-}
