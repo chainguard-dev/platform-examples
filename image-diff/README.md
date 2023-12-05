478eb35b442736678fab6a7676665bb9b88199a2
Jason Hall <jason@chainguard.dev>
2023-12-05 14:37:59 -0700

    support real auth, not just public images
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- image-diff/README.md
+++ image-diff/README.md
@@ -8,7 +8,9 @@ Along with Tag History, you can use this to show the evolution of an image over 
 
 
 ```sh
-previous=sha256:0b7ee54f5b593bd9463cc56a37d16b447de4ea95492dba9cfa5ce2d732e6352c
-current=sha256:c894bc454800817b1747c8a1a640ae6d86004b06190f94e791098e7e78dbbc00
-go run ./cmd/app go $previous $current
+group=customer.biz # defaults to public images
+reponame=kubeflow-pipelines-frontend
+previous=sha256:00d89a080976cad0fb94e1c1629c9f3a4d8d424572ba590955147348c80a0473
+current=sha256:54382392baa1e116f3540c6b1c96c7ae441d06b38504e8be1546b85c9405e94b
+go run ./cmd/app --group=$group $reponame $previous $current
 ```
--- image-diff/cmd/app/main.go
+++ image-diff/cmd/app/main.go
@@ -13,8 +13,11 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"time"
 
+	"chainguard.dev/sdk/auth/token"
 	common "chainguard.dev/sdk/proto/platform/common/v1"
+	iam "chainguard.dev/sdk/proto/platform/iam/v1"
 	registry "chainguard.dev/sdk/proto/platform/registry/v1"
 	"chainguard.dev/sdk/sts"
 	"github.com/google/go-containerregistry/pkg/name"
@@ -22,6 +25,8 @@ import (
 
 func main() {
 	ctx := context.Background()
+	var group string
+	flag.StringVar(&group, "group", "chainguard", "group to query")
 	flag.Parse()
 
 	if len(flag.Args()) != 3 {
@@ -39,46 +44,80 @@ func main() {
 	var tok string
 	audience := "https://console-api.enforce.dev"
 	{
-		// This group is special, since anybody can access it by assuming a
-		// broadly-assumable identity with permission to view/pull.
+		if group == "chainguard" {
+			// This group is special, since anybody can access it by assuming a
+			// broadly-assumable identity with permission to view/pull.
 
-		issuer := "https://issuer.enforce.dev"
-		resp, err := http.Get("https://justtrustme.dev/token?aud=" + issuer)
-		if err != nil {
-			log.Fatalf("getting justtrustme token: %v", err)
-		}
-		defer resp.Body.Close()
-		if resp.StatusCode != http.StatusOK {
-			log.Fatalf("getting justtrustme token: %v", resp.Status)
-		}
-		all, err := io.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatalf("reading justtrustme token: %v", err)
-		}
-		var r struct {
-			Token string `json:"token"`
-		}
-		if err := json.Unmarshal(all, &r); err != nil {
-			log.Fatalf("decoding justtrustme token: %v", err)
-		}
-		tok = r.Token
+			issuer := "https://issuer.enforce.dev"
+			resp, err := http.Get("https://justtrustme.dev/token?aud=" + issuer)
+			if err != nil {
+				log.Fatalf("getting justtrustme token: %v", err)
+			}
+			defer resp.Body.Close()
+			if resp.StatusCode != http.StatusOK {
+				log.Fatalf("getting justtrustme token: %v", resp.Status)
+			}
+			all, err := io.ReadAll(resp.Body)
+			if err != nil {
+				log.Fatalf("reading justtrustme token: %v", err)
+			}
+			var r struct {
+				Token string `json:"token"`
+			}
+			if err := json.Unmarshal(all, &r); err != nil {
+				log.Fatalf("decoding justtrustme token: %v", err)
+			}
+			tok = r.Token
 
-		tok, err = sts.New(issuer, audience,
-			sts.WithIdentity("720909c9f5279097d847ad02a2f24ba8f59de36a/a033a6fabe0bfa0d")).
-			Exchange(ctx, tok)
-		if err != nil {
-			log.Fatalf("exchanging token: %v", err)
+			tok, err = sts.New(issuer, audience,
+				sts.WithIdentity("720909c9f5279097d847ad02a2f24ba8f59de36a/a033a6fabe0bfa0d")).
+				Exchange(ctx, tok)
+			if err != nil {
+				log.Fatalf("exchanging token: %v", err)
+			}
+		} else {
+			if token.RemainingLife(audience, time.Minute) < 0 {
+				// TODO: do a browser flow here.
+				log.Fatalf("token has expired, please run `chainctl auth login`")
+			}
+			tokb, err := token.Load(audience)
+			if err != nil {
+				log.Fatalf("loading token: %v", err)
+			}
+			tok = string(tokb)
 		}
 	}
 
-	// Set up client.
+	// Set up clients.
+	iamc, err := iam.NewClients(ctx, audience, tok)
+	if err != nil {
+		log.Fatalf("creating IAM clients: %v", err)
+	}
 	regc, err := registry.NewClients(ctx, audience, tok)
 	if err != nil {
 		log.Fatalf("creating Registry clients: %v", err)
 	}
 
-	// This group is special, we'll just hard-code the UIDP.
-	groupUIDP := "720909c9f5279097d847ad02a2f24ba8f59de36a"
+	// Get the group UIDP.
+	var groupUIDP string
+	{
+		if group == "chainguard" {
+			// This group is special, we'll just hard-code the UIDP.
+			groupUIDP = "720909c9f5279097d847ad02a2f24ba8f59de36a"
+		} else {
+			resp, err := iamc.Groups().List(ctx, &iam.GroupFilter{
+				Name: group,
+			})
+			if err != nil {
+				log.Fatalf("listing groups: %v", err)
+			}
+			if len(resp.Items) != 1 {
+				log.Fatalf("expected 1 group, got %d", len(resp.Items))
+			}
+			groupUIDP = resp.Items[0].Id
+		}
+	}
+	log.Println("group UIDP", groupUIDP)
 
 	// Get the repo UIDP.
 	var repoUIDP string
@@ -108,5 +147,10 @@ func main() {
 	if err != nil {
 		log.Fatalf("diff: %v", err)
 	}
-	fmt.Println(resp)
+
+	b, err := json.MarshalIndent(resp, "", "  ")
+	if err != nil {
+		log.Fatalf("marshaling response: %v", err)
+	}
+	fmt.Println(string(b))
 }
