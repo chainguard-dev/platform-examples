94e6c62693366c11ff8cf6a85ee084f86a7d3316
Andrew Dean <andrew.dean@chainguard.dev>
2025-08-05 17:06:39 +1000

    Fixed typo

--- image-copy-crane-bash-script/mirror-images.sh
+++ image-copy-crane-bash-script/mirror-images.sh
@@ -4,14 +4,14 @@ trap "echo 'Interrupted. Exiting...'; exit 1" SIGINT SIGTERM
 
 AWS_PAGER=""
 AWS_REGION="ap-southeast-2"
-TARGET_ECR_REPO_PREFIX="/chainguard" # Change this to your desired ECR repo prefix or leave empty for root
+TARGET_ECR_REPO_PREFIX="chainguard" # Change this to your desired ECR repo prefix
 TARGET_ECR="452336408843.dkr.ecr.ap-southeast-2.amazonaws.com"
 CG_ORG="andrewd.dev"
 
 chainctl images repos list --parent="$CG_ORG" -o json \
   | jq -r '.items[].name' \
   | while read -r image; do
-    echo "Creating ECR repo: $TARGET_ECR_REPO_PREFIX/$image"
+    echo "Creating ECR repo: $TARGET_ECR$TARGET_ECR_REPO_PREFIX/$image"
     aws ecr create-repository \
       --region $AWS_REGION \
       --repository-name "$TARGET_ECR_REPO_PREFIX/$image" \
@@ -22,7 +22,7 @@ chainctl images repos list --parent="$CG_ORG" -o json \
     chainctl img ls --parent="$CG_ORG" --repo "$image" -o json \
       | jq -r '.[].tags[].name' \
       | while read -r tag; do
-          echo "#### Copying: cgr.dev/$CG_ORG/${image}:${tag} -> $TARGET_ECR$TARGET_ECR_REPO_PREFIX/${image}:${tag} ####"
-          crane cp "cgr.dev/$CG_ORG/${image}:${tag}" "$TARGET_ECR$TARGET_ECR_REPO_PREFIX/${image}:${tag}"
+          echo "#### Copying: cgr.dev/$CG_ORG/${image}:${tag} -> $TARGET_ECR/$TARGET_ECR_REPO_PREFIX/${image}:${tag} ####"
+          crane cp "cgr.dev/$CG_ORG/${image}:${tag}" "$TARGET_ECR/$TARGET_ECR_REPO_PREFIX/${image}:${tag}"
         done
   done
